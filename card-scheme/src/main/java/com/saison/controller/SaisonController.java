9fc8bb5f9e2f7f1755f70684bc4d67193a780334
garima <garima@garima-Latitude-E7450>
2021-04-13 10:03:38 +0530

    format code

--- card-scheme/src/main/java/com/saison/controller/SaisonController.java
+++ card-scheme/src/main/java/com/saison/controller/SaisonController.java
@@ -23,7 +23,8 @@ public class SaisonController {
 
 	@Autowired
 	AdminDao adminDao;
-	final String uri = "https://lookup.binlist.net/" ;
+	final String uri = "https://lookup.binlist.net/";
+
 	@RequestMapping("/test")
 	public void abc(HttpServletRequest reques, HttpServletResponse response) {
 		System.out.println("running");
@@ -35,22 +36,22 @@ public class SaisonController {
 		String url = uri + id;
 		RestTemplate restTemplate = new RestTemplate();
 		String result = restTemplate.getForObject(url, String.class);
-		
-		System.out.println("id:"+id+"  result:"+result);
-		
+
+		System.out.println("id:" + id + "  result:" + result);
+
 		JSONObject updateDb = adminDao.saveCard(result, id);
 		JSONObject payload = new JSONObject();
 		payload.put("scheme", updateDb.get("scheme"));
 		payload.put("type", updateDb.get("type"));
 		payload.put("bank", updateDb.get("bank"));
-		
+
 		JSONObject responseObj = new JSONObject();
-		
-		if(updateDb.get("status").equals(1)) {
+
+		if (updateDb.get("status").equals(1)) {
 			responseObj.put("success", true);
 			responseObj.put("payload", payload);
-			
-		}else {
+
+		} else {
 			responseObj.put("success", false);
 		}
 		response.setContentType("application/json");
@@ -59,23 +60,24 @@ public class SaisonController {
 		pw.flush();
 		pw.close();
 	}
-	
+
 	@RequestMapping("stats")
-	public void showStats(HttpServletRequest request, HttpServletResponse response, @RequestParam Integer start, @RequestParam Integer limit) throws IOException {
+	public void showStats(HttpServletRequest request, HttpServletResponse response, @RequestParam Integer start,
+			@RequestParam Integer limit) throws IOException {
 		System.out.println(limit);
 		System.out.println(start);
 		int totalStats = adminDao.totalStats();
-		JSONObject getData = adminDao.getStats(start, limit); 
+		JSONObject getData = adminDao.getStats(start, limit);
 		JSONObject responseObj = new JSONObject();
 
-		if(getData.get("status").equals(1)) {
+		if (getData.get("status").equals(1)) {
 			responseObj.put("success", true);
 			responseObj.put("start", start);
 			responseObj.put("limit", limit);
 			responseObj.put("size", totalStats);
 			responseObj.put("payload", getData.get("payload"));
-			
-		}else {
+
+		} else {
 			responseObj.put("success", false);
 		}
 		response.setContentType("application/json");
@@ -83,7 +85,5 @@ public class SaisonController {
 		pw.print(responseObj);
 		pw.flush();
 		pw.close();
-	
-		
 	}
 }
